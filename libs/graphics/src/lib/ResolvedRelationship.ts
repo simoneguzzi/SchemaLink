6d69a49f42710ce0a9964d34a7c54dfaf1327a6e
Simone Guzzi <simone.guzzi@sib.swiss>
2024-09-13 16:12:17 +0200

    feat(graphics): add new exports

--- libs/graphics/src/lib/ResolvedRelationship.ts
+++ libs/graphics/src/lib/ResolvedRelationship.ts
@@ -1,14 +1,16 @@
-import { EntitySelection, Graph, Relationship } from '@neo4j-arrows/model';
+import { Graph, Relationship } from '@neo4j-arrows/model';
 import { VisualNode } from './VisualNode';
 import { VisualAttachment } from './VisualAttachment';
 
 export class ResolvedRelationship {
-  type: any;
   // id(selection: EntitySelection, id: any): any {
   //   throw new Error("Method not implemented.");
   // }
   get id() {
     return this.relationship.id;
+  }
+  get type() {
+    return this.relationship.type;
   }
   constructor(
     readonly relationship: Relationship,
