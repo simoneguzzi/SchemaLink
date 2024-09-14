34e4a9eb5b837502ad2a21f4bb45f27d62de653b
Simone Guzzi <simone.guzzi@sib.swiss>
2024-09-14 04:28:22 +0200

    refactor(model): improve typing

--- libs/model/src/lib/guides/guides.ts
+++ libs/model/src/lib/guides/guides.ts
@@ -1,14 +1,15 @@
 import { Point } from '../Point';
+import { AnyGuide } from './AnyGuide';
 
 export const byAscendingError = (a: { error: number }, b: { error: number }) =>
   a.error - b.error;
 
 export class Guides {
-  guidelines: any[];
+  guidelines: AnyGuide[];
   naturalPosition?: Point;
   naturalRadius?: number;
   constructor(
-    guidelines = [],
+    guidelines: AnyGuide[] = [],
     naturalPosition: Point | undefined = undefined,
     naturalRadius: number | undefined = undefined
   ) {
